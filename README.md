23842eb250e72309b8abc9217f17c05c2e710ff5
jimboscatfood <149596389+jimboscatfood@users.noreply.github.com>
2025-07-20 11:59:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# template
+A template repository with basic set up code structure and dependencies including:
+- A html file, a js file and css file in the src directory for working
+- A main.js file for bundling in the dist directory
+- A package.json file with necessary devDependencies including html plugins and css loader
+- Config for webpack
+- Config for eslint
+- Config for prettier
+
+Run "npm install" in new repo for initialisation
---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/entry.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+};
